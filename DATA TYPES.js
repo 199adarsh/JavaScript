e38ff22f060b8d1e74749205244e0c20d4972389
199adarsh <adarshherwade11214@gmail.com>
2025-06-11 07:48:48 +0530

    Initial commit

---
+++ DATA TYPES.js
@@ -0,0 +1,16 @@
+//alert(2+2)
+
+let name = "xyz" //string
+let age = 18 // number
+let loggedIn = true // boolean
+
+
+// bigInt used when the number is grater than 5^53
+// null - standerd value
+//undefined - 
+// symbol - used mostly in react for uniqueness
+
+//object 
+console.log(typeof null);   
+
+console.log(typeof undefined)
---
+++ main.js
@@ -0,0 +1,5 @@
+let accountId = 1224
+const accountPass = 9292  
+var accountName = "Adarsh" // dont use var very frequently becouse it can cause issues 
+console.log(accountId)
+console.table([accountPass,accountId,accountName]) 
