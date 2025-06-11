70cb4adfad3a3130386c8497d27551b5a0ea0e47
199adarsh <adarshherwade11214@gmail.com>
2025-06-11 17:29:41 +0530

    commit5

--- Comparison.js
+++ Comparison.js
@@ -1,6 +1,17 @@
 console.log(2 > 1); // answers come in boolean True/False
 console.log(3 >= 2);
 console.log(3 <= 2);
+console.log()
 
-console.log("2" > 1);
-  
+console.log("2" > 1); // cpnverts the string into number and checks the operation 
+console.log()
+
+
+//avoid this below codes as it is not a clean code 
+console.log(null >= 0)
+console.log(null == 0)
+console.log(null > 0)
+
+console.log()
+console.log("2" === 2) // Triple = compares the data types also 
+// in this case the one data type is string and other one is number
